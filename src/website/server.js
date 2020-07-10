422850bfc059422bc2941fa66fddebb22519568a
ondralukes <mail@ondralukes.cz>
2020-07-10 18:27:30 +0200

    404 page

--- src/website/server.js
+++ src/website/server.js
@@ -3,6 +3,10 @@ var app = express();
 
 const port = 8080;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.use((req, res) => {
+    res.status(404).sendFile('public/404.html', {root: __dirname});
+});
 
 var server = app.listen(port);
