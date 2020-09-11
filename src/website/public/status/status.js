b1d8002f5d88b6d228523e602b45a418016ba81b
ondralukes <mail@ondralukes.cz>
2020-09-11 09:25:00 +0200

    Updated /status to use new API

--- src/website/public/status/status.js
+++ src/website/public/status/status.js
@@ -1,8 +1,8 @@
 function fetchServices(){
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function(){
-    if(xhr.readyState == 4 && xhr.status == 200){
-      addServices(JSON.parse(xhr.responseText));
+    if(xhr.readyState === 4 && xhr.status === 200){
+      addServices(JSON.parse(xhr.responseText).services);
     }
   };
   xhr.open("GET", "/rawstatus", true);
@@ -10,15 +10,15 @@ function fetchServices(){
 }
 
 function addServices(services){
-  var template = document.getElementById('serv-template');
+  const template = document.getElementById('serv-template');
   services.forEach((service) => {
-    var clone = template.cloneNode(true);
+    const clone = template.cloneNode(true);
     clone.id = 'serv-' + service.name;
     clone.style.display = "";
     clone.getElementsByClassName('serv-name')[0].innerHTML = service.name;
     clone.getElementsByClassName('serv-url')[0].innerHTML = service.url;
     clone.getElementsByClassName('serv-target')[0].innerHTML = service.target;
-    var reachableElement = clone.getElementsByClassName('serv-reachable')[0];
+    const reachableElement = clone.getElementsByClassName('serv-reachable')[0];
     if(service.reachable){
       reachableElement.innerHTML = "Yes";
       reachableElement.classList.add('font-weight-bold');
@@ -28,12 +28,12 @@ function addServices(services){
       reachableElement.classList.add('font-weight-bold');
       reachableElement.classList.add('text-danger');
     }
-    var containerElement = clone.getElementsByClassName('serv-container')[0];
-    var containerRowTemplate = containerElement.getElementsByClassName('serv-container-row')[0];
+    const containerElement = clone.getElementsByClassName('serv-container')[0];
+    const containerRowTemplate = containerElement.getElementsByClassName('serv-container-row')[0];
     if(typeof service.container !== 'undefined'){
-      var keys = Object.keys(service.container);
+      const keys = Object.keys(service.container);
       keys.forEach((key) => {
-        var row = containerRowTemplate.cloneNode(true);
+        const row = containerRowTemplate.cloneNode(true);
         row.style.display = "";
         row.getElementsByClassName('serv-container-key')[0].innerHTML = key;
         row.getElementsByClassName('serv-container-val')[0].innerHTML = service.container[key];
